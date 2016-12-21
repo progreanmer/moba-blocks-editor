2b5a07b6ffc27ccf373c5f8c51df8cf08d7d6a34
progreanmer <greanmer.tm@gmail.com>
2016-12-21 18:10:27 +0700

    add -save block to xml format file -open xml format file to blocks

--- src/main.js
+++ src/main.js
@@ -1,17 +1,27 @@
 
-var code = 'from sim_monitor.sim_client.api_game.hero_controller import HeroController\n'
-+'from sim_monitor.sim_client.api_game.my_hero import MyHero\n'
-+'import time\n\n\n'
-+ 'class x(MyHero):\n' + '    def __init__(self):\n' + '        super().__init__()\n'
-+ '        self.controller = HeroController()\n'
-+ '        time.sleep(5)\n\n' +'    def run(self):\n'
-+ '        ' + Blockly.Python.workspaceToCode(workspace);
 
 function showCode() {
+
+  var code = 'from sim_monitor.sim_client.api_game.hero_controller import HeroController\n'
+  +'from sim_monitor.sim_client.api_game.my_hero import MyHero\n'
+  +'import time\n\n\n'
+  + 'class x(MyHero):\n' + '    def __init__(self):\n' + '        super().__init__()\n'
+  + '        self.controller = HeroController()\n'
+  + '        time.sleep(5)\n\n' +'    def run(self):\n'
+  + '        ' + Blockly.Python.workspaceToCode(workspace);
+
   alert(code);
 }
 
 function exportCode() {
+
+  var code = 'from sim_monitor.sim_client.api_game.hero_controller import HeroController\n'
+  +'from sim_monitor.sim_client.api_game.my_hero import MyHero\n'
+  +'import time\n\n\n'
+  + 'class x(MyHero):\n' + '    def __init__(self):\n' + '        super().__init__()\n'
+  + '        self.controller = HeroController()\n'
+  + '        time.sleep(5)\n\n' +'    def run(self):\n'
+  + '        ' + Blockly.Python.workspaceToCode(workspace);
 
   Blockly.Python.INFINITE_LOOP_TRAP = null;
 
@@ -31,3 +41,46 @@ function exportCode() {
       gen_file.click();
   }
 }
+
+function saveBlock() {
+  var xml = Blockly.Xml.workspaceToDom(workspace);
+  var xml_text = Blockly.Xml.domToPrettyText(xml)
+
+  Blockly.Python.INFINITE_LOOP_TRAP = null;
+
+  var gen_file = document.createElement('a');
+  var file_name = document.getElementById('fname').value
+
+
+   gen_file.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(xml_text));
+   gen_file.setAttribute('download',file_name);
+
+  if (document.createEvent) {
+      var event = document.createEvent('MouseEvents');
+      event.initEvent('click', true, true);
+      gen_file.dispatchEvent(event);
+  }
+  else {
+      gen_file.click();
+  }
+}
+
+function openBlock(raw_file) {
+  var xml = Blockly.Xml.textToDom(raw_file);
+  Blockly.Xml.domToWorkspace(xml, workspace);
+}
+
+function readSingleFile(e) {
+  var file = e.target.files[0];
+  if (!file) {
+    return;
+  }
+  var reader = new FileReader();
+  reader.onload = function(e) {
+    var contents = e.target.result;
+    openBlock(contents);
+  };
+  reader.readAsText(file);
+}
+document.getElementById('file-input')
+  .addEventListener('change', readSingleFile, false);
