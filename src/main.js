e9f6003b41f7ae61b916956cc4305197bb59d0ed
progreanmer <greanmer.tm@gmail.com>
2017-04-17 02:00:28 +0700

    -add rate function(in testing)

--- src/main.js
+++ src/main.js
@@ -96,15 +96,105 @@ function rate(){
   console.log(name);
 
   var rfr = new XMLHttpRequest();
-  rfr.open('GET', name , false);
+  rfr.open('GET', "log/" + name , false);
   rfr.send('');
 
-  log = rfr.responseText;
+  var txt = rfr.responseText;
+  var lst = txt.split("\n");
+  var word=[],time=[];
+  var atk=0, dead=0, kill=0, end_time=0, start_time=0, tot_time;
+  var r_kill, r_survive, r_fight, r_push, r_cpm;
+  for(sen in lst){
+    word = sen.split(" ");
+    for(sw in word){
+      if(sw == "ATK"){
+        atk += 1;
+      }
+      else if(sw == "DIED"){
+        dead += 1;
+      }
+      else if(sw == "USE_SKILL"){
+        atk += 1;
+      }
+      else if(sw == "KILL"){
+        kill += 1;
+      }
+      else if(sw == "team1 win" || sw == "team2 win"){
+        time = word[0].split(":");
+        var hrs = parseInt(word[0]);
+        var mins = parseInt(word[1]);
+        var sec = parseInt(word[2]);
+        end_time = ( hrs * 3600 ) + ( mins * 60) + sec;
+      }
+      else if(sw == "game start"){
+        time = word[0].split(":");
+        var hrs = parseInt(word[0]);
+        var mins = parseInt(word[1]);
+        var sec = parseInt(word[2]);
+        start_time = ( hrs * 3600 ) + ( mins * 60) + sec;
+      }
+    }
+  }
+  tot_time = end_time - start_time;
+  r_kill = rate_kill(kill, tot_time);
+  r_survive = rate_survive(dead, tot_time);
+  r_fight = rate_fight(atk, tot_time);
+  r_push = rate_push(tot_time);
+  r_cpm = rate_performance(kill, dead);
 
-  console.log(log);
+  // console.log(log);
 
 }
 
+//function for calculate point of kill
+function rate_kill(kill,time){
+  if((kill/time) >= (1/120)){  //assume perfect code can kill more than or equal 30 heroes in 1 hours
+    return 10.00;
+  }
+  else{
+    return ((kill/time) * 1200);
+  }
+}
+
+//function for calculate point of survive
+function rate_survive(dead,time){
+  if((time/dead) <= (360)){ //assume Perfect code dead must dead lower than or equal 6 in 1 hours
+    return 10.00;
+  }
+  else{
+    return ((time/dead)/36);
+  }
+}
+
+//function for calculate point of fight
+function rate_fight(atk, time){
+  if((atk/time) >= (1/20)){
+    return 10.00;
+  }
+  else{
+    return ((atk/time)*200);
+  }
+}
+
+//function for calculate point of push
+function rate_push(time){
+  if(time <= 1200){  // time at most 20 miniutes
+    return 10.00;
+  }
+  else if(time >1200){ //time more than 20 minutes
+    return (10*(1200/time));
+  }
+}
+
+function rate_performance(kill,dead){
+  var ratio = kill/dead
+  if(ratio >= 1){
+    return "GOOD";
+  }
+  else{
+    return "BAD";
+  }
+}
 // read xml file for loading to category
 
 var xhrObj = new XMLHttpRequest();
